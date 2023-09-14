9b91b95d5c3324a8dad7ac51f27fbb836aa06f0b
Ethanan1 <98491284+Ethanan1@users.noreply.github.com>
2023-09-14 16:09:26 -0700

    Login functionaliy setup tokens:

--- my-spotify/src/App.js
+++ my-spotify/src/App.js
@@ -1,10 +1,43 @@
 import './App.css';
 import Login from './Login'
+import { getTokenFromResponse } from './spotify';
+import React, { useEffect, useState } from 'react';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken ] = useState(null);
+
+  //run code based of condition
+  useEffect(() => {
+    const hash = getTokenFromResponse();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        console.log('user:', user);
+      })
+    }
+
+    console.log('I HAVE TOKEN', token);
+  }, []);
+
   return (
     <div className="app">
-    <Login></Login>
+      {
+        token ? (
+          <h1>I am logged in</h1>
+        ) : (
+          <Login></Login>
+        )
+      }
+
     </div>
   );
 }
--- my-spotify/src/spotify.js
+++ my-spotify/src/spotify.js
@@ -9,13 +9,13 @@ const scopes = [
   "user-top-read",
   "user-modify-playback-state",
 ];
-    
+
 export const getTokenFromResponse = () => {
   return window.location.hash
     .substring(1)
     .split("&")
     .reduce((initial, item) => {
-      var parts = item.split("=");
+      let parts = item.split("=");
       initial[parts[0]] = decodeURIComponent(parts[1]);
 
       return initial;
