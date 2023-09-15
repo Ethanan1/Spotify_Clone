f6675e8f1b6205fa341a10ec7e9162e764bde26e
Ethanan1 <98491284+Ethanan1@users.noreply.github.com>
2023-09-14 17:14:29 -0700

    Datalayer bugs

--- my-spotify/src/App.js
+++ my-spotify/src/App.js
@@ -4,11 +4,13 @@ import { getTokenFromResponse } from './spotify';
 import React, { useEffect, useState } from 'react';
 import SpotifyWebApi from 'spotify-web-api-js';
 import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
 
 const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken ] = useState(null);
+  const [{user}, dispatch] = useDataLayerValue(); //allows us to pull from data layer
 
   //run code based of condition
   useEffect(() => {
@@ -23,11 +25,20 @@ function App() {
 
       spotify.getMe().then(user => {
         console.log('user:', user);
+
+        dispatch({
+          type: 'SET_USER_',
+          user: user,
+
+        })
+
       })
     }
 
     console.log('I HAVE TOKEN', token);
   }, []);
+
+  console.log("USER: ", user);
 
   return (
     <div className="app">
